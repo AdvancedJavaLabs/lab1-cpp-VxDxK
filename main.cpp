5d796792d41db81662e95a66219c85d6dc61acdd
Vadim Ponomarev <vadim.v.ponomarev@gmail.com>
2025-10-14 02:14:23 +0300

    Add solution with results

--- main.cpp
+++ main.cpp
@@ -6,21 +6,24 @@
 #include "Graph.h"
 #include "RandomGraphGenerator.h"
 
-static long long executeSerialBfsAndGetTime(Graph& g) {
+static long long executeSerialBfsAndGetTime(Graph &g)
+{
     auto start = std::chrono::steady_clock::now();
     g.bfs(0);
     auto end = std::chrono::steady_clock::now();
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
 
-static long long executeParallelBfsAndGetTime(Graph& g) {
+static long long executeParallelBfsAndGetTime(Graph &g)
+{
     auto start = std::chrono::steady_clock::now();
-    g.parallelBFS(0); // заглушка
+    g.parallelBFS(0);
     auto end = std::chrono::steady_clock::now();
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
 
-int main() {
+int main()
+{
     try {
         std::vector<int> sizes       = {10, 100, 1000, 10000, 10000, 50000, 100000, 1000000, 2000000, 20000000};
         std::vector<int> connections = {50, 500, 5000, 50000, 100000, 1000000, 1000000, 10000000, 10000000, 50000000};
@@ -44,15 +47,21 @@ int main() {
             long long serialTime = executeSerialBfsAndGetTime(g);
             long long parallelTime = executeParallelBfsAndGetTime(g);
 
+#if 1
             fw << "Times for " << sizes[i] << " vertices and " << connections[i] << " connections: ";
             fw << "\nSerial: " << serialTime;
             fw << "\nParallel: " << parallelTime;
             fw << "\n--------\n";
             fw.flush();
+#else
+            // fw << '"' << sizes[i] << "/" << connections[i] << "\": (" << serialTime << "," << parallelTime << "),\n";
+            fw << parallelTime << "";
+            fw.flush();
+#endif
         }
 
         std::cout << "Done. Results in tmp/results.txt\n";
-    } catch (const std::exception& ex) {
+    } catch (const std::exception &ex) {
         std::cerr << "Exception: " << ex.what() << "\n";
         return 2;
     }
